f49459fb0ffa62c94b784164ca83c56d29759b37
Aljaaiz <aljaaiz1@gmail.com>
2018-11-23 15:16:36 +0400

    little information added

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ let color1 = document.querySelector('.color1');
 let color2 = document.querySelector('.color2');
 let h3 = document.getElementsByTagName('h3')[0];
 let body = document.getElementById('gradient');
+let hexcode = document.getElementById('hexcode');
 
 function gradientGene() {
 	body.style.background = 'linear-gradient(to right, ' + color1.value + ',' + color2.value + ')';
 	h3.textContent = body.style.background + ';';
+	hexcode.textContent = `Hexcode Value : ${color1.value} , ${color2.value}`;
 }
 color1.addEventListener('input', gradientGene);
 color2.addEventListener('input', gradientGene);
